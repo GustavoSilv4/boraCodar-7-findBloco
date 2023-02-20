f620725d23dba050feb7c1e5c92f23e4d3264dd7
Gustavo Silva <gustavosilv4.contato@gmail.com>
2023-02-20 14:59:15 -0300

    feat: Add new color for theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -2,6 +2,7 @@ export const defaultTheme = {
   white: "#FFF",
   black: "#000",
   purple: "#6246EA",
+  "gray-100": "#F8F8FF",
   "gray-300": "#7C7C8A",
   "gray-900": "#121214",
   "red-200": "#E45858",
